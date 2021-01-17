f6933a009d09671b7194675a10710d1f0084370a
Barthes Simpson <d.s.frenk@gmail.com>
2021-01-17 14:15:40 -0800

    Responsive

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,27 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { useState, useLayoutEffect } from "react";
+
+function getIsLandscape() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  return width > height;
+}
 
 export default function Home() {
+  const [isLandscape, setIsLandscape] = useState(false);
+
+  useLayoutEffect(() => {
+    function onWindowResize() {
+      setIsLandscape(getIsLandscape());
+    }
+    window.addEventListener("resize", onWindowResize);
+    return () => {
+      window.removeEventListener("resize", onWindowResize);
+    };
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,12 +30,16 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Image
-          src="/EmCat2.png"
-          alt="A sad cat eating the late Emily"
-          layout="fill"
-          style={{ objectFit: "cover" }}
-        />
+        {isLandscape ? (
+          <Image
+            src="/EmCat2.png"
+            alt="A sad cat eating the late Emily"
+            layout="fill"
+            style={{ objectFit: "cover" }}
+          />
+        ) : (
+          <h1>Please turn your phone sideways</h1>
+        )}
       </main>
 
       <footer className={styles.footer}></footer>
